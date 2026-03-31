5b3427fd0182f07632c8547efc9e10c90e50ee1a
Jayami Sulakshi <100301026+jayamisulakshi@users.noreply.github.com>
2026-03-31 22:20:17 +0000

    error message

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,5 @@ sudo apt-get update
 sudo apt-get install sl
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
 echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
+echo '❌ Hey @professortocat, I'\''ve finished testing out my new Codespace. I'\''m ready to review!' && exit 1
+
